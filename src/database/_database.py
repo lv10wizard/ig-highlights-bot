b0446c5bd9b448bf75223f2542fced78dfa72142
lv10wizard <lv10wizard@gmail.com>
2017-09-11 10:11:51 -0700

    [BROKEN] Move database module to directory
    This allows the individual database child classes to live in their own
    files.
    
    database/__init__.py exposes every sub-module attribute defined in
    that sub-module's __all__ list. This is done so that src-level modules
    can import as 'import database' then use as if it were a single module,
        eg. database.ReplyDatabase()

--- src/database/_database.py
+++ src/database/_database.py
@@ -142,65 +142,9 @@ class Database(object):
     @abc.abstractmethod
     def udpate(self, *args, **kwargs): pass
 
-class ReplyDatabase(Database):
-    """
-    Storage of all replies made by the bot
-    """
-
-    @property
-    def _create_table_data(self):
-        return (
-                'comments('
-                '   comment_id TEXT PRIMARY KEY'
-                '   submission_id TEXT NOT NULL,'
-                '   ig_user TEXT NOT NULL,'
-                # apply unique constraint on specified keys
-                # https://stackoverflow.com/a/15822009
-                '   UNIQUE(submission_id, ig_user)'
-                ')'
-        )
-
-    def _insert(self, comment, ig):
-        with self._db as connection:
-            connection.execute(
-                    'INSERT INTO comments(comment_id, submission_id, ig_user)'
-                    ' VALUES(?, ?, ?)',
-                    (comment.id, comment.submission.id, ig.user.lower()),
-            )
-
-    def replied_comments_for_submission(self, submission_id):
-        """
-        Returns a set of comment ids that the bot has replied to for a given
-        post
-        """
-        cursor = self._db.execute(
-                'SELECT comment_id FROM comments WHERE submission_id = ?',
-                (submission_id,),
-        )
-        return set([row['comment_id'] for row in cursor])
-
-    def replied_ig_users_for_submission(self, submission_id):
-        """
-        Returns the set of instagram user names that the bot has replied with
-        for a given post
-        """
-        cursor = self._db.execute(
-                'SELECT ig_user FROM comments WHERE submission_id = ?',
-                (submission_id,),
-        )
-        return set([row['ig_user'] for row in cursor])
-
-    def has_replied(self, comment):
-        """
-        Returns True if the bot has replied to the specified comment
-        """
-        cursor = self._db.execute(
-                'SELECT comment_id FROM comments WHERE comment_id = ?',
-                (comment.id,),
-        )
-        return bool(cursor.fetchone())
-
 
 __all__ = [
+        'FailedInit',
+        'Database',
 ]
 
