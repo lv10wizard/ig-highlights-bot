aaf65c1e0d659c008f0b7a36b3b037fbeb9b1514
lv10wizard <lv10wizard@gmail.com>
2017-10-20 10:53:04 +1100

    Fix 3ad797a not printing links

--- args.py
+++ args.py
@@ -397,10 +397,10 @@ def print_instagram_database(cfg, order, *user_databases):
 def print_instagram_database_links(cfg, order, *user_databases):
     def do_print_links(path, order):
         import sqlite3
-        sqlite3.row_factory = sqlite3.Row
 
         if os.path.exists(path):
             db = sqlite3.connect(path)
+            db.row_factory = sqlite3.Row
             cursor = db.execute('SELECT link FROM cache {0}'.format(order))
             for row in cursor:
                 print(row['link'])
