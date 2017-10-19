0127edb3bc2cfe3b39e4adbfef167af3e6f6c091
lv10wizard <lv10wizard@gmail.com>
2017-10-20 07:16:43 +1100

    Add 'yowzers' jargon

--- tests/replies/test_parser.py
+++ tests/replies/test_parser.py
@@ -44,7 +44,8 @@ from src.replies import Parser
     'mothaaaafuckaaaa', 'mothrrrfuhhhckrrrrr', 'shaddup', 'shaattttup',
     'shutup', 'shuuuuutaahhhhhp', 'suckaaaaahhh', 'suckaaa', 'mothaasuckaaaa',
     'suqqqqquuuuhhhh', 'suckerrrr', 'awwwwhhh', 'ahhhhhhwwww', 'ahhhhh',
-    'awwwwww', 'riiiiip', 'rrrriipppppppppp', 'oomph', 'oooomf',
+    'awwwwww', 'riiiiip', 'rrrriipppppppppp', 'oomph', 'oooomf', 'yowzers',
+    'yowzas', 'yowsers', 'yowzerrrz', 'yowzah',
 ] + ['o'*(i+2) for i in range(15)])
 def test_parser_detects_jargon(word):
     assert Parser.is_jargon(word)
