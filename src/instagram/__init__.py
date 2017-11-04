27436a1ccca79dcbbeb240a04edba6cd07bbfc54
lv10wizard <lv10wizard@gmail.com>
2017-11-04 01:08:42 -0700

    [BROKEN?] Implement instagram._init()
    This is intended as an interpreter testing convenience method.

--- src/instagram/__init__.py
+++ src/instagram/__init__.py
@@ -1,5 +1,14 @@
 from src.util.modules import expose_modules
 
 
+def _init():
+    """
+    Interpreter testing initialize()
+    """
+    from .instagram import initialize
+    from src import config
+
+    initialize(config.Config(), '/u/igHighlightsBot')
+
 __all__ = expose_modules(__file__, __name__, locals())
 
