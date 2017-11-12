lv10wizard/ig-highlights-bot

commit af6d172cc36bc30fe651a7220b168669b4a7efdb

Implement submitting posts to bot's profile
* Note: this commit is far too large in size and should have been
  implemented in its own branch.

This commit will dramatically increase the bot's instagram network
footprint (as long as the submit_enabled setting is on).

The majority of the changes can be separated into two somewhat distinct
sections:
        1. submitter & userpool implementation
                The submitter runs in its own process and attempts to
                limit reposting duplicates (both user duplicates and
                per-user link duplicates).

        2. reddit ratelimit handling

* Note #2: this commit adds columns to the RedditRateLimitQueueDatabase
  meaning existing versions of this database prior to this change will
  be wiped.

USER_POOL
@@ -1,0 +1,233 @@
+# This file contains a list of usernames (one per line) that the bot
+# will post to its profile subreddit if it has one.
+#
+# Empty lines and anything after a '#' are ignored. Duplicates are also
+# ignored.
+
+elizabethcturner
+alexisren
+sierraaaskyee
+yoventura
+jenselter
+annanystrom
+sommerray
+sommerray2
+morganlux
+fullmetalifrit
+elliecowelll
+k.01.bulgakova
+jadegrobler
+_cassiebrown_
+vyvan.le
+fox_in_a_bawkes
+galinda_dub
+stephxohaven
+hannahpbyrd
+happylittlesprout
+nikinikiii
+sexicacti
+myfaerielove
+saraunderwood
+niecewaidhofer
+jemwolfie
+leannadecker_
+veronicabielik
+juli.annee
+helga_model
+miss_tina_louise
+itskristinachai
+mollyeskam
+anacheri
+lisapeachy
+verabambilive
+eliselaurenne
+thezoebaron
+noelleoninsta
+haylienoire
+monifajansen
+indialove
+celielindblad
+thelittleti
+southernchivette91
+angievarona
+lindseypelas
+jessicababyfat
+veronikablack88
+heavenuhh
+_ashh_cashhh
+abrunamagalhaes
+robincyn
+tasya.krivun
+jenna_jadecb
+alice_posse
+_jessdavies
+vickibaybeee
+demirosemawby
+cindyprado
+parisdylan550
+yessibabbby
+mirgaeva_galinka
+baconbootyy
+lovialongtime
+darealginavalentina
+therealkendrasunderland
+officialumajolie
+arianamarie15_
+strangerthangranger
+lissapolooza
+msmaplefever
+xoalexgrey
+2cheekzback
+meganloveonme
+rejaniced
+dangershewrote
+elsajeanofficial
+bratnasty69
+kali.roses
+cali.peachez
+viki_odintcova
+vi_odintcova
+connie.shtt
+isizzu
+savvy_rose
+sophiemudd
+luciajavorcekova
+alexa.pearl
+_queenfrankie
+jenafrumes
+allisoncalvs
+casidavis
+katyaelisehenry
+maddy
+thaihava
+rolyatistaylor
+_coffeecutie
+milaazul
+eugeniadior
+nancy_anastasiia
+little_caprice_dreams
+metart.official
+petterhegre
+saracalixtocr
+rayanizhoni
+emrata
+nakedbl0ndeee
+lucycatofficial
+jillianjansonentertainment
+megansalina
+sia89
+misstoriblack
+omgitslexi
+thesydneycole
+joselinekellyx
+karla_kush420
+alexagraceofficial
+red_fox_official1
+piper_perri
+cobramfc
+octaviamaysg
+alexapond_
+darinalytvyn
+harli_lotts
+fiona.bl
+valentivitell
+ibellaan
+christywithak
+hells_fitness
+karmabirdfly
+avakoor
+slawadall
+missviho
+missjuliakelly
+martina_finocchio
+ini.helen
+xsabina_21
+polinasitnova
+emblu
+
+triippyunicorn
+celenawithac
+allyalv_
+moriahbecker
+jessicambartlett
+yevaleva
+tigerlily.taylor
+mfayyy
+kvthrym
+theashleydee
+bfknaniston
+nicki_andrea
+lkirkk
+olivia.x.o
+sofia_bevarly
+dreysdiary
+pani
+taylorkayteee
+christenmcallister
+christina_belanger
+christiana.mora
+kayrosedavies
+andressabarb
+kinsey_sue
+emilyyyyjonessss
+steph_misko
+tasha.farsaci
+lyssieelou_
+ajozkoski
+natfarnella
+kristinamichelleeee
+calitrent
+sarinaaalauren
+juliaamill
+julia__yaroshenko
+atamuratova
+talibanani
+yuliyabb
+dasha_lashes
+nannalund1
+zuueva
+victoria.ku
+elen.dali
+__baby_898
+jenna.charlette
+brittanydanyelle0
+gabssxo
+maryjuy
+ksenia_wegner
+alexandra_ermolaeva
+liya_michelle
+nastya115
+kardelian
+krasivchik_ksusha
+fitness_junkie91
+pingvidii
+sofa_s
+juliagreenemerald
+marcie_mft
+sazhina
+angelica_anders
+oxanastreltsova
+bugs_bunny_gf
+kery__lee
+vikkimartynova
+dianagherghel
+elya1p
+ninaserebrova
+sofia_official_
+katya_ogo
+anyuta_rai
+kristabelkrayt
+anya.sugar
+ornellu_mutit
+katya.aivazova
+
+tfmgirls
+ganjagirls
+edmbabes_
+paradise_baristas
+paradise_espresso
+pinksugarladies
+pinksugarbaristas
+suicidegirls
+fshmidt
+

src/database/reddit_ratelimit.py
@@ -20,10 +20,19 @@ class RedditRateLimitQueueDatabase(Database):
     @staticmethod
     def fullname(thing):
         try:
+            # try subreddit.display_name
+            return thing.display_name
+        except AttributeError:
+            pass
+
+        try:
+            # try .fullname
             return thing.fullname
         except AttributeError:
-            # probably fullname string
-            return thing
+            pass
+
+        # hopefully fullname string
+        return thing
 
     def __contains__(self, thing):
         cursor = self._db.execute(
@@ -39,47 +48,122 @@ class RedditRateLimitQueueDatabase(Database):
                 '   uid INTEGER PRIMARY KEY NOT NULL,'
                 '   fullname TEXT NOT NULL,'
                 '   submission_fullname TEXT,'
-                '   body TEXT NOT NULL,'
+                '   body TEXT,' # comment/submission reply
+                '   title TEXT,' # submit title
+                '   selftext TEXT,' # submit selftext
+                '   url TEXT,' # submit url
                 '   ratelimit_reset REAL NOT NULL,'
-                '   UNIQUE(fullname, body)'
+                '   UNIQUE(fullname, body, title, selftext, url)'
                 ')'
         )
 
-    def _insert(self, thing, body, ratelimit_delay, submission=None):
+    def _insert(
+            self, thing, ratelimit_delay, body=None, title=None,
+            selftext=None, url=None, submission=None
+    ):
+        """
+        Inserts a thing to reply/post-to to be handled once the bot is no
+        longer reddit ratelimited.
+
+        thing (praw.models.*) - the thing to reply or post to
+        ratelimit_delay (float) - the amount of time in seconds until the
+                ratelimit is over
+        body (str, optional) - the body to reply with
+        title (str, optional) - the title of the post to submit
+        selftext (str, optional) - the selftext of the post to submit
+        url (str, optional) - the url of the post to submit
+        submission (praw.models.Submission, optional) -
+                the submission which contains the thing (if any)
+
+        *Note: body & title/selftext/url are mutually exclusive
+        """
+
+        if bool(body) == bool(title or selftext or url):
+            # programmer error
+            logger.id(logger.warn, self,
+                    'Mutually exclusive parameters \'body\''
+                    ' & (\'title\' or \'selftext\' or \'url\') both specified!'
+                    ' NOT inserting \'{color_thing}\' ...',
+                    color_thing=RedditRateLimitQueueDatabase.fullname(thing),
+            )
+            if body:
+                logger.id(logger.debug, self,
+                        'body:\n\n{body}\n',
+                        body=body,
+                )
+            if title:
+                logger.id(logger.debug, self,
+                        'title:\n\n{title}\n',
+                        title=title,
+                )
+            if selftext:
+                logger.id(logger.debug, self,
+                        'selftext:\n\n{selftext}\n',
+                        selftext=selftext,
+                )
+            if url:
+                logger.id(logger.debug, self,
+                        'url:\n\n{url}\n',
+                        url=url,
+                )
+            # skip (and lose) data instead of raising an error
+            return
+
         self._db.execute(
                 'INSERT INTO'
-                ' queue(fullname, submission_fullname, body, ratelimit_reset)'
-                ' VALUES(?, ?, ?, ?)',
+                ' queue(fullname, submission_fullname, body, title,'
+                '       selftext, url, ratelimit_reset)'
+                ' VALUES({0})'.format(', '.join(['?'] * 7)),
                 (
                     RedditRateLimitQueueDatabase.fullname(thing),
                     RedditRateLimitQueueDatabase.fullname(submission),
                     body,
+                    title,
+                    selftext,
+                    url,
                     time.time() + ratelimit_delay,
                 ),
         )
         self.__update_has_elements()
 
-    def _delete(self, thing, body):
+    def _delete(self, thing, body=None, title=None, selftext=None, url=None):
         self._db.execute(
-                'DELETE FROM queue WHERE fullname = ? AND body = ?',
+                'DELETE FROM queue WHERE fullname = ?'
+                ' AND body = ?'
+                ' AND title = ?'
+                ' AND selftext = ?'
+                ' AND url = ?',
                 (
                     RedditRateLimitQueueDatabase.fullname(thing),
                     body,
+                    title,
+                    selftext,
+                    url,
                 ),
         )
         self.__update_has_elements()
 
-    def _update(self, thing, body, ratelimit_delay):
+    def _update(
+            self, thing, ratelimit_delay, body=None, title=None,
+            selftext=None, url=None,
+    ):
         """
         Updates the ratelimit-reset time for the thing
         """
         self._db.execute(
                 'UPDATE queue SET ratelimit_reset = ?'
-                ' WHERE fullname = ? AND body = ?',
+                ' WHERE fullname = ?'
+                ' AND body = ?'
+                ' AND title = ?'
+                ' AND selftext = ?'
+                ' AND url = ?',
                 (
                     time.time() + ratelimit_delay,
                     RedditRateLimitQueueDatabase.fullname(thing),
                     body,
+                    title,
+                    selftext,
+                    url,
                 ),
         )
 
@@ -153,9 +237,12 @@ class RedditRateLimitQueueDatabase(Database):
             queue.Empty is raised if either block == False or timeout is a
             positive number if no item was available.
 
-        Returns (fullname, body) of the first element where ratelimit_reset is
-                less than the current time.time(); ie, the first element that
-                is no longer rate-limited.
+        Returns tuple (fullname, body, title, selftext, url) of the first
+                element where ratelimit_reset is less than the current
+                time.time(); ie, the first element that is no longer
+                rate-limited. (Note that only body or title/selftext/url will
+                have values -- eg. if body has a value then title, selftext and
+                url will be None.)
 
                 or None if all elements are still rate-limited and the timeout
                 has expired.
@@ -224,7 +311,16 @@ class RedditRateLimitQueueDatabase(Database):
                     # still rate-limited but not blocking
                     row = None
 
-        return (row['fullname'], row['body']) if row else None
+        if row:
+            return (
+                    row['fullname'],
+                    row['body'],
+                    row['title'],
+                    row['selftext'],
+                    row['url'],
+            )
+
+        return None
 
 
 __all__ = [

src/ratelimit.py
@@ -274,6 +274,142 @@ class RateLimitHandler(ProcessMixin, RedditInstanceMixin):
         self.__rate_limit_proc.start()
         ProcessMixin.start(self)
 
+    def _handle_reply(self, fullname, body):
+        """
+        Handles a reddit ratelimit queued reply
+
+        Returns True if a reply was attempted for the thing
+        """
+        handled = False
+        thing = self._reddit.get_thing_from_fullname(fullname)
+        if thing:
+            logger.id(logger.info, self,
+                    'Processing {color_thing} ...',
+                    color_thing=reddit.display_id(thing),
+            )
+            # only handle specific types of things
+            if isinstance(thing, RateLimitHandler.VALID_THINGS):
+                logger.id(logger.info, self,
+                        'Replying to {color_thing} ...',
+                        color_thing=reddit.display_id(thing),
+                )
+
+                # Note: we may be rate-limited again
+                success = self._reddit.do_reply(
+                        thing, body, self._killed,
+                )
+
+                if success or success is None:
+                    # reply either succeeded or a reply is not possible
+                    # (eg. 403 Forbidden)
+                    # remove the element from the queue database
+                    with self.rate_limit_queue:
+                        self.rate_limit_queue.delete(thing, body=body)
+
+                if success:
+                    # try to add the thing to the reply history
+                    # (but only if we can find instagram users
+                    #  in the body)
+                    ig_users = replies.Formatter.ig_users_in(
+                            body
+                    )
+                    if ig_users:
+                        try:
+                            with self.reply_history:
+                                self.reply_history.insert(
+                                        thing, ig_users,
+                                )
+
+                        except database.UniqueConstraintFailed:
+                            display = reddit.display_id(
+                                    thing
+                            )
+                            logger.id(logger.warn, self,
+                                    'Duplicate instagram user'
+                                    ' posted in'
+                                    ' {color_submission}!'
+                                    ' (users={color_users})',
+                                    color_submission=display,
+                                    color_users=ig_users,
+                                    exc_info=True,
+                            )
+                handled = True
+
+        return handled
+
+    def _handle_submit(self, display_name, title, selftext, url):
+        """
+        Handles a reddit ratelimit queued submit
+
+        Returns True if a submit was attempted
+        """
+        logger.id(logger.info, self,
+                'Posting \'{title}\' to {subname} ...',
+                title=title,
+                subname=display_name,
+        )
+        if selftext:
+            logger.id(logger.debug, self,
+                    'selftext:\n\n{selftext}\n',
+                    selftext=selftext,
+            )
+        if url:
+            logger.id(logger.debug, self,
+                    'url:\n\n{url}\n',
+                    url=url,
+            )
+
+        success = self._reddit.do_submit(
+                display_name=display_name,
+                title=title,
+                selftext=selftext,
+                url=url,
+                killed=self._killed,
+        )
+
+        if success or success is None:
+            # submit succeeded or could not be made
+            # remove the element from the queue database
+            with self.rate_limit_queue:
+                self.rate_limit_queue.delete(
+                        thing=display_name,
+                        title=title,
+                        selftext=selftext,
+                        url=url,
+                )
+
+        # XXX: there is currently no case where the queued item is not handled
+        return True
+
+    def _log_element(self, element, msg_prefix=''):
+        if logger.is_enabled_for(logger.DEBUG):
+            _, body, title, selftext, url = element
+
+            if body:
+                logger.id(logger.debug, self,
+                        '{prefix}body:\n\n{body}\n',
+                        prefix=msg_prefix,
+                        body=body,
+                )
+            if title:
+                logger.id(logger.debug, self,
+                        '{prefix}title:\n\n{title}\n',
+                        prefix=msg_prefix,
+                        title=title,
+                )
+            if selftext:
+                logger.id(logger.debug, self,
+                        '{prefix}selftext:\n\n{selftext}\n',
+                        prefix=msg_prefix,
+                        selftext=selftext,
+                )
+            if url:
+                logger.id(logger.debug, self,
+                        '{prefix}url:\n\n{url}\n',
+                        prefix=msg_prefix,
+                        url=url,
+                )
+
     def _run_forever(self):
         from src import reddit
 
@@ -292,67 +428,28 @@ class RateLimitHandler(ProcessMixin, RedditInstanceMixin):
                 continue
 
             if element:
-                fullname, body = element
+                fullname, body, title, selftext, url = element
 
-                thing = self._reddit.get_thing_from_fullname(fullname)
-                if thing:
-                    logger.id(logger.info, self,
-                            'Processing {color_thing} ...',
-                            color_thing=reddit.display_id(thing),
+                if bool(body) == bool(title or selftext or url):
+                    # too many content strings queued; cannot determine
+                    # proper reddit method to call
+                    logger.id(logger.warn, self,
+                            'Cannot handle \'{color_fullname}\':'
+                            ' too many content strings!',
+                            color_fullname=fullname,
                     )
-                    # only handle specific types of things
-                    if isinstance(thing, RateLimitHandler.VALID_THINGS):
-                        logger.id(logger.info, self,
-                                'Replying to {color_thing} ...',
-                                color_thing=reddit.display_id(thing),
-                        )
+                    self._log_element(element)
 
-                        # Note: we may be rate-limited again
-                        success = self._reddit.do_reply(
-                                thing, body, self._killed,
-                        )
+                elif body:
+                    handled = self._handle_reply(fullname, body)
 
-                        if success or success is None:
-                            # reply either succeeded or a reply is not possible
-                            # (eg. 403 Forbidden)
-                            # remove the element from the queue database
-                            with self.rate_limit_queue:
-                                self.rate_limit_queue.delete(thing, body)
-
-                        if success:
-                            # try to add the thing to the reply history
-                            # (but only if we can find instagram users
-                            #  in the body)
-                            ig_users = replies.Formatter.ig_users_in(
-                                    body
-                            )
-                            if ig_users:
-                                try:
-                                    with self.reply_history:
-                                        self.reply_history.insert(
-                                                thing, ig_users,
-                                        )
-
-                                except database.UniqueConstraintFailed:
-                                    display = reddit.display_id(
-                                            thing
-                                    )
-                                    logger.id(logger.warn, self,
-                                            'Duplicate instagram user'
-                                            ' posted in'
-                                            ' {color_submission}!'
-                                            ' (users={color_users})',
-                                            color_submission=display,
-                                            color_users=ig_users,
-                                            exc_info=True,
-                                    )
-                        handled = True
+                elif title:
+                    handled = self._handle_submit(
+                            fullname, title, selftext, url
+                    )
 
                 if not handled:
-                    logger.id(logger.debug, self,
-                            'Unhandled body:\n{body}\n\n',
-                            body=body,
-                    )
+                    self._log_element(element, msg_prefix='Unhandled ')
 
                     # remove the element so that it isn't immediately retried
                     logger.id(logger.warn, self,
@@ -360,7 +457,13 @@ class RateLimitHandler(ProcessMixin, RedditInstanceMixin):
                             color_thing=reddit.display_id(thing),
                     )
                     with self.rate_limit_queue:
-                        self.rate_limit_queue.delete(thing, body)
+                        self.rate_limit_queue.delete(
+                                thing=thing,
+                                body=body,
+                                title=title,
+                                selftext=selftext,
+                                url=url,
+                        )
 
             # else:
             #     # get() timed out, queue not empty

commit 82f07bb83013df86620b517fa831d9f565681398

Fix RedditRateLimitQueueDatabase not deleting rows
This was an issue introduced in af6d172. SQLite treats NULLs as distinct
values for UNIQUE constraints and WHERE clauses. This was causing
strange behavior when dealing with the database such as rows not being
deleted.

This commit resolves the issue by replacing NULL (None) with a
placeholder value that SQLite should treat as a non-distinct value.

See https://www.sqlite.org/nulls.html for more information about how
SQLite handles NULLs.

USER_POOL
@@ -145,6 +145,7 @@ xsabina_21
 polinasitnova
 emblu
 gardenofguise
+ziennasonne
 
 triippyunicorn
 celenawithac

src/database/reddit_ratelimit.py
@@ -15,6 +15,10 @@ class RedditRateLimitQueueDatabase(Database):
 
     PATH = 'reddit-queue.db'
 
+    # XXX: a non-NULL value to insert into the database since SQLite treats
+    # NULL as a distinct value; ie, UNIQUE constraints and WHERE clauses
+    _NO_VALUE = 'NONE'
+
     __has_elements = multiprocessing.Event()
 
     @staticmethod
@@ -34,6 +38,12 @@ class RedditRateLimitQueueDatabase(Database):
         # hopefully fullname string
         return thing
 
+    @staticmethod
+    def _replace_null(value):
+        if value is None:
+            return RedditRateLimitQueueDatabase._NO_VALUE
+        return value
+
     def __contains__(self, thing):
         cursor = self._db.execute(
                 'SELECT fullname FROM queue WHERE fullname = ?',
@@ -48,10 +58,10 @@ class RedditRateLimitQueueDatabase(Database):
                 '   uid INTEGER PRIMARY KEY NOT NULL,'
                 '   fullname TEXT NOT NULL,'
                 '   submission_fullname TEXT,'
-                '   body TEXT,' # comment/submission reply
-                '   title TEXT,' # submit title
-                '   selftext TEXT,' # submit selftext
-                '   url TEXT,' # submit url
+                '   body TEXT NOT NULL,' # comment/submission reply
+                '   title TEXT NOT NULL,' # submit title
+                '   selftext TEXT NOT NULL,' # submit selftext
+                '   url TEXT NOT NULL,' # submit url
                 '   ratelimit_reset REAL NOT NULL,'
                 '   UNIQUE(fullname, body, title, selftext, url)'
                 ')'
@@ -117,10 +127,10 @@ class RedditRateLimitQueueDatabase(Database):
                 (
                     RedditRateLimitQueueDatabase.fullname(thing),
                     RedditRateLimitQueueDatabase.fullname(submission),
-                    body,
-                    title,
-                    selftext,
-                    url,
+                    RedditRateLimitQueueDatabase._replace_null(body),
+                    RedditRateLimitQueueDatabase._replace_null(title),
+                    RedditRateLimitQueueDatabase._replace_null(selftext),
+                    RedditRateLimitQueueDatabase._replace_null(url),
                     time.time() + ratelimit_delay,
                 ),
         )
@@ -135,10 +145,10 @@ class RedditRateLimitQueueDatabase(Database):
                 ' AND url = ?',
                 (
                     RedditRateLimitQueueDatabase.fullname(thing),
-                    body,
-                    title,
-                    selftext,
-                    url,
+                    RedditRateLimitQueueDatabase._replace_null(body),
+                    RedditRateLimitQueueDatabase._replace_null(title),
+                    RedditRateLimitQueueDatabase._replace_null(selftext),
+                    RedditRateLimitQueueDatabase._replace_null(url),
                 ),
         )
         self.__update_has_elements()
@@ -160,10 +170,10 @@ class RedditRateLimitQueueDatabase(Database):
                 (
                     time.time() + ratelimit_delay,
                     RedditRateLimitQueueDatabase.fullname(thing),
-                    body,
-                    title,
-                    selftext,
-                    url,
+                    RedditRateLimitQueueDatabase._replace_null(body),
+                    RedditRateLimitQueueDatabase._replace_null(title),
+                    RedditRateLimitQueueDatabase._replace_null(selftext),
+                    RedditRateLimitQueueDatabase._replace_null(url),
                 ),
         )
 
@@ -312,12 +322,17 @@ class RedditRateLimitQueueDatabase(Database):
                     row = None
 
         if row:
+            def replace_with_none(value):
+                if value == RedditRateLimitQueueDatabase._NO_VALUE:
+                    return None
+                return value
+
             return (
-                    row['fullname'],
-                    row['body'],
-                    row['title'],
-                    row['selftext'],
-                    row['url'],
+                    replace_with_none(row['fullname']),
+                    replace_with_none(row['body']),
+                    replace_with_none(row['title']),
+                    replace_with_none(row['selftext']),
+                    replace_with_none(row['url']),
             )
 
         return None

src/ratelimit.py
@@ -300,6 +300,11 @@ class RateLimitHandler(ProcessMixin, RedditInstanceMixin):
                 )
 
                 if success or success is None:
+                    logger.id(logger.debug, self,
+                            'Removing \'{color_thing}\' from'
+                            ' reddit ratelimit queue ...',
+                            color_thing=thing,
+                    )
                     # reply either succeeded or a reply is not possible
                     # (eg. 403 Forbidden)
                     # remove the element from the queue database
@@ -368,6 +373,12 @@ class RateLimitHandler(ProcessMixin, RedditInstanceMixin):
         )
 
         if success or success is None:
+            logger.id(logger.debug, self,
+                    'Removing \'{color_thing}\': \'{title}\' from'
+                    ' reddit ratelimit queue ...',
+                    color_thing=display_name,
+                    title=title,
+            )
             # submit succeeded or could not be made
             # remove the element from the queue database
             with self.rate_limit_queue:

src/submitter.py
@@ -142,7 +142,7 @@ class Submitter(ProcessMixin, RedditInstanceMixin):
                                 '\n\ttotal:           #{total}'
                                 '\n\tnon-highlighted: #{non_highlighted}'
                                 '\n\tpostable-links:  #{postable_links}'
-                                ' Choosing another user ...',
+                                '\nChoosing another user ...',
                                 color_user=user,
                                 unique_count=unique_count,
                                 total=(