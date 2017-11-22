389011c98a7313b8bf31fabe5c36ff72a227efbf
lv10wizard <lv10wizard@gmail.com>
2017-11-22 11:09:43 -0800

    Add 'yummers' jargon (wtf is this word)

--- tests/replies/test_parser.py
+++ tests/replies/test_parser.py
@@ -64,7 +64,7 @@ from src.replies import Parser
     'cutie3.14', 'qt3.14159265359', 'cutie3.14159265359', 'qtpie', 'goodie',
     'goody', 'gooooody', 'gooooodieieieie', 'goodiebag', 'goodiebaaaagggg',
     'bangin', 'banging', 'baaaaangin', 'banggggginggggg', 'xd', 'XD', 'XDDDD',
-    'xdddddddddddddd', 'xxxdddddddddddddddddDDDdd',
+    'xdddddddddddddd', 'xxxdddddddddddddddddDDDdd', 'yummers', 'yummerzz',
 ] + ['o'*(i+2) for i in range(15)])
 def test_parser_detects_jargon(word):
     assert Parser.is_jargon(word)
